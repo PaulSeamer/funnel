6c60429a646cca49236165c9ad495e0e2f96cf26
PaulSeamer <paulseamer@nhs.net>
2017-08-14 15:09:46 +0100

    better object names and tidy comments

--- funlData.R
+++ funlData.R
@@ -37,7 +37,6 @@ funl_Data <- function(df.data, col.unit, col.group, col.O, col.n, col.rt
   
   # if a value for target is not supplied then use global (weighted) mean
   if (is.null(target)) {
-      # target = global mean
       target <- sum(df.data[[col.O]], na.rm = TRUE) / sum(df.data[[col.O]] / df.data[[col.rt]], na.rm = TRUE)
   } else {
     target = target
@@ -45,41 +44,43 @@ funl_Data <- function(df.data, col.unit, col.group, col.O, col.n, col.rt
   
   # implement method for selecting smoothness factor if none supplied?
 
-  # if values for fnlMinEvents and fnlMaxEvents are not supplied then calculate sensible values
-  # i.e. how far should the funnels extend to the left/right beyond the min/max number of the denominator n in df.data
+  # determines x-axis extent of funnels
+  # calculate values for fnlMinEvents and fnlMaxEvents (if none provided in call)
   if (is.null(fnlMinEvents)) {
-      fnlMinEvents <- rounddown_Nice(min(df.data[[col.O]], na.rm = TRUE)) 
+    fnlMinEvents <- rounddown_Nice(min(df.data[[col.O]], na.rm = TRUE))
   }
   if (is.null(fnlMaxEvents)) {
-      fnlMaxEvents <- roundup_Nice(max(df.data[[col.O]], na.rm = TRUE))
+    fnlMaxEvents <- roundup_Nice(max(df.data[[col.O]], na.rm = TRUE))
   }
   
   # produce funnels
   if (smoothness > diff(range(fnlMinEvents, fnlMaxEvents))) {
       
-    # if value for smoothness supplied in call to funl_Data is greater than maxEvents - minEvents then
-    # recalibrate smoothness to maxEvents - minEvents
+    # if value for smoothness supplied in call to funl_Data (default = 100)
+    # is greater than maxEvents - minEvents
+    # then recalibrate smoothness to maxEvents - minEvents
     events <- seq.int(fnlMinEvents, fnlMaxEvents)
     
-  } else {
+    } else {
     
     # else initialise empty vector to hold events and set first element to minEvents
     events    <- rep(NA_real_, smoothness)
     events[1] <- fnlMinEvents
     
-    # use for loop to fill events vector where the difference to the preceeding elements value gradually increases
+    # for loop to fill events vector where the difference to the preceeding elements value gradually increases
     for (i in seq_len(smoothness)[-1]) {
       
       events[i] <- max(round((fnlMaxEvents / events[i - 1])^(1 / ((smoothness + 1) - i)) * events[i - 1]), events[i - 1])
-      
+    
     }
+    
   }
   
   # derive denominator n
   n <- events / target
 
-  # nested df (events series nested in confidence levels)
-  x <- expand.grid(fnlLimit = c("threeSigma", "twoSigma")
+  # nested df (events series and confidence levels)
+  fnlInput <- expand.grid(fnlLimit = c("threeSigma", "twoSigma")
                    , events = events
                    , stringsAsFactors = FALSE) %>% 
     mutate(conf.level = ifelse(fnlLimit == "threeSigma", 0.99, 0.95)) %>% 
@@ -90,9 +91,9 @@ funl_Data <- function(df.data, col.unit, col.group, col.O, col.n, col.rt
   
   # calculate confidence intervals with exact Poisson test
   # poisson.test - x must be x must be finite, non-negative, and integer  
-  # apply poisson.test to each event value in nested df
+  # apply poisson.test to each combinaton of event values and confidence levels in nested df
   # use broom::tidy to store results in dfs
-  y <- x %>%
+  fnlOutput <- fnlInput %>%
     group_by(fnlLimit) %>%
     mutate(
       pois = map(data, ~ map2_df(.$events, .$conf.level, function(x, y)
@@ -101,8 +102,8 @@ funl_Data <- function(df.data, col.unit, col.group, col.O, col.n, col.rt
     ungroup() %>% 
     unnest(pois)
  
-  # prepare funnel df to return
-  outA <- y %>%
+  # tidy fnl df
+  funls <- fnlOutput %>%
     select(estimate, conf.low, conf.high, fnlLimit) %>%
     mutate(
       target = target
@@ -114,26 +115,24 @@ funl_Data <- function(df.data, col.unit, col.group, col.O, col.n, col.rt
     rename(
       events = estimate
     )
-  
-  # this was just ported over - may be a better way to do this
-  # create second df of units and if in-control T/F
-  outB <- data.frame(df.data[[col.unit]], stringsAsFactors = FALSE)
-  names(outB) <- col.unit
+
+  # create df for units and in-control T/F
+  units <- as_tibble(df.data[[col.unit]], stringsAsFactors = FALSE)
+  names(units) <- col.unit
   
   # apply poisson.test to observed event values in input df
-  outB$threeSigmaLow <- sapply(df.data[[col.O]], function(x) if(is.na(x)) {NA}
+  units$threeSigmaLow <- sapply(df.data[[col.O]], function(x) if(is.na(x)) {NA}
                                else{poisson.test(x, alternative = "two.sided", conf.level = 0.99)$conf.int[1]}) / (df.data[[col.O]] /target)
   
-  outB$threeSigmaHigh <- sapply(df.data[[col.O]], function(x) if(is.na(x)) {NA}
+  units$threeSigmaHigh <- sapply(df.data[[col.O]], function(x) if(is.na(x)) {NA}
                                 else {poisson.test(x, alternative = "two.sided", conf.level = 0.99)$conf.int[2]} ) / (df.data[[col.O]] / target)
   
-  outB$status <- ifelse(df.data[[col.rt]] < outB$threeSigmaLow, "low", ifelse(df.data[[col.rt]] > outB$threeSigmaHigh, "high", "in"))
+  units$status <- ifelse(df.data[[col.rt]] < units$threeSigmaLow, "low", ifelse(df.data[[col.rt]] > units$threeSigmaHigh, "high", "in"))
     
-  # return list of outA & outB
-  outA <- as_tibble(outA)
-  outB <- as_tibble(outB)
-  out <- list(outA, outB)
-  return(out)
+  # return list of fnl & units
+  fnl <- as_tibble(fnlOut)
+  results <- list(fnl, units)
+  return(results)
   
   }
 
